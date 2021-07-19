9de801012c5ed1a3f0554ca62014f6b4717751f3
Vinzent Brömauer <vinz.corno@web.de>
2021-07-19 21:38:59 +0200

    remove test code

--- Program.cs
+++ Program.cs
@@ -7,15 +7,6 @@ namespace GB
     {
         static void Main(string[] args)
         {
-            ushort value = 0xFFFF;
-            byte value2 = (byte)value;
-
-            byte[] values = new byte[2];
-            values[0] = 0xff;
-
-            Console.WriteLine("short value: {0}, byte value: {1}", value, value2);
-            Console.WriteLine("byte shifted: {0}", value2 << 8);
-            Console.WriteLine("byte 3: {0}, added 1: {1}", values[0], (byte)(values[0] + 1));
             using (var g = new Controller())
                 g.Run();
         }
