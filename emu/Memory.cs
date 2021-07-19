6a9fcfb2123052e6a9809db2149774af4e04b2ab
Vinzent Brömauer <vinz.corno@web.de>
2021-07-19 21:20:24 +0200

    add constants to memory areas

--- emu/Memory.cs
+++ emu/Memory.cs
@@ -6,7 +6,24 @@ namespace GB.emu
 {
     public class Memory
     {
-        private byte[] mem = new byte[32];
+        //BANK0 starts at 0x00 so it's left out here
+        public const ushort BANK1 = 0x4000;
+        public const ushort VRAM = 0x8000;
+        public const ushort ExRAM = 0xA000;
+        public const ushort WRAM0 = 0xC000;
+        public const ushort WRAM1 = 0xD000;
+        public const ushort ERAM = 0xE000; //echo ram, mirror of both WRAMS
+        public const ushort SAT = 0xFE00;
+        public const ushort HiRAM = 0xFF80;
+        public const ushort IEREG = 0xFFFF;
+
+        public byte IE
+        {
+            get => mem[0xFFFF];
+            set => mem[0xFFFF] = value;
+        }
+
+        private byte[] mem = new byte[0xFFFF];
 
         public byte this[uint index]
         {
