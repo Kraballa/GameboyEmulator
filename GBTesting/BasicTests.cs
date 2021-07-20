2cd199d1c465508f1f93fdf9d780df048a366cf3
Vinzent Brömauer <vinz.corno@web.de>
2021-07-20 11:02:00 +0200

    add test framework

---
+++ GBTesting/BasicTests.cs
@@ -0,0 +1,18 @@
+﻿using GB.emu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBTesting
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void TestEmptyRom()
+        {
+            TestCPU cpu = new TestCPU(Rom.Empty);
+        }
+    }
+}
