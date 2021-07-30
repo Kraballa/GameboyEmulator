9d9da0193c4759a61595eb2b963d4b03415bb0ec
Vinzent Brömauer <vinz.corno@web.de>
2021-07-30 12:05:04 +0200

    print out debug info for OAM transfer

--- GB/emu/Memory.cs
+++ GB/emu/Memory.cs
@@ -130,7 +130,7 @@ namespace GB.emu
         /// </summary>
         private void OamDmaTransfer(byte written)
         {
-            Console.WriteLine("start oam transfer");
+            Console.WriteLine("start oam transfer at address 0x{0:X}", written);
             ushort start = (ushort)(written << 8);
             for (ushort offset = 0x00; offset < 0x9F; offset++)
             {
