7ba0ed531735a58f96a630d8cdd63dd627c26811
Vinzent Brömauer <vinz.corno@web.de>
2021-08-09 14:23:32 +0200

    define method attributes

---
+++ GBTesting/Attributes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBTesting
+{
+    //denotes a method to be run before every test
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SetupAttribute : Attribute
+    {
+    }
+
+    //denotes a method to be run after every test
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CleanupAttribute : Attribute
+    {
+    }
+
+    //denotes a test method
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TestAttribute : Attribute
+    {
+    }
+}
